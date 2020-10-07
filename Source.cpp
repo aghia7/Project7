604716e4040af466b282c93386be490ad0fc52d5
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2020-10-07 09:23:39 +0600

    isPrime function

--- Source.cpp
+++ Source.cpp
@@ -18,6 +18,15 @@ char getSign(int a, int b) {
 	return '=';
 }
 
+bool isPrime(int n) {
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
 int main()
 {
 	cout << compare << endl;
